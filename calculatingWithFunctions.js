523f564ddcbb041fe89369d8818e23e2a9c8dbe1
Himavarsha Golla <himavarsha_golla@epam.com>
2021-04-28 15:58:24 +0530

    Refactor code.

--- calculatingWithFunctions.js
+++ calculatingWithFunctions.js
@@ -1,205 +1,97 @@
 // write calculations using functions and get the results
 // Eg: seven(times(five())); // must return 35, four(plus(nine())); // must return 13
 function zero() {
+    const num = 0;
     if(arguments.length == 0) {
-      return 0;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 0;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
+      return num;
     }
+    return getResult(arguments, num); 
 }
 
 function one() {
+    const num = 1;
     if(arguments.length == 0) {
-        return 1;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 1;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
+        return num;
     }
+    return getResult(arguments, num); 
 }
 
 function two() {
     if(arguments.length == 0) {
         return 2;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 2;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 2); 
 }
 
 function three() {
     if(arguments.length == 0) {
         return 3;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 3;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 3); 
 }
 
 function four() {
     if(arguments.length == 0) {
         return 4;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 4;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 4); 
 }
 
 function five() {
     if(arguments.length == 0) {
         return 5;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 5;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 5); 
 }
 
 function six() {
     if(arguments.length == 0) {
         return 6;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 6;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 6); 
 }
 
 function seven() {
     if(arguments.length == 0) {
         return 7;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 7;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 7); 
 }
 
 function eight() {
     if(arguments.length == 0) {
         return 8;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 8;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
     }
+    return getResult(arguments, 8); 
 }
 
 function nine() {
     if(arguments.length == 0) {
         return 9;
-    } else {
-        var operation = arguments[0][0];
-        var operand1 = 9;
-        var operand2 = arguments[0][1];
-        switch(operation) {
-            case '*':
-                return operand1 * operand2;
-            case '+':
-                return operand1 + operand2;
-            case '-':
-                return operand1 - operand2;
-            case '/':
-                return Math.floor(operand1 / operand2);
-        }
+    } 
+    return getResult(arguments, 9);    
+}
+
+function getResult(args, num) {
+    var operation = args[0][0];
+    var operand1 = num;
+    var operand2 = args[0][1];
+    return getOperationResult(operation, operand1, operand2);
+}
+
+function getOperationResult(operation, operand1, operand2) {
+    switch(operation) {
+        case '*':
+            return operand1 * operand2;
+        case '+':
+            return operand1 + operand2;
+        case '-':
+            return operand1 - operand2;
+        case '/':
+            return Math.floor(operand1 / operand2);
     }
 }
-  
+
 function plus() {
     return ["+", arguments[0]];
 }
